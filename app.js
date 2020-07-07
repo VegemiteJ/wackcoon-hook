1bacb5734be93c22a0b0e5d6b871b2db201e325b
VegemiteJ <>
2020-07-06 22:14:23 -0700

    Update repo details

--- app.js
+++ app.js
@@ -2,48 +2,54 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var exec = require('child_process').exec;
+// Read config
+var config = require('config');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.htm');
-	console.log('get /');
+    console.log('get /');
 });
 
 app.get('/payload', function (req, res) {
     res.sendStatus(200);
-	console.log('get /payload');
+    console.log('get /payload');
 });
 
+// Handle webhook request
 app.post('/payload', function (req, res) {
-	//verify that the payload is a push from the correct repo
-	//verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
-	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
+    //verify that the payload is a push from the correct repo
+    //verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
+    console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.full_name);
 
-	console.log('pulling code from GitHub...');
+    // Read local config file with project name and path
+    const projectName = config.get('projectName');
+    const projectPath = config.get('projectPath');
+    console.log('Read name and path... Name:' + projectName + ', Path:' + projectPath)
+    if (req.body.repository.name !== projectName) {
+        console.log('Rejecting request - repository name does not match config')
+        res.sendStatus(404);
+        return;
+    };
 
-	// reset any changes that have been made locally
-	exec('git -C ~/projects/wackcoon-device reset --hard', execCallback);
+    console.log('Pulling code from GitHub...');
+    // reset any changes that have been made locally
+    //exec('git -C ' + projectPath + ' reset --hard', execCallback);
 
-	// and ditch any files that have been added locally too
-	exec('git -C ~/projects/wackcoon-device clean -df', execCallback);
+    // and ditch any files that have been added locally too
+    //exec('git -C ' + projectPath + ' clean -xdf', execCallback);
 
-	// now pull down the latest
-	exec('git -C ~/projects/wackcoon-device pull -f', execCallback);
-
-	// and npm install with --production
-	exec('npm -C ~/projects/wackcoon-device install --production', execCallback);
-
-	// and run tsc
-	exec('tsc', execCallback);
+    // now pull down the latest
+    //exec('git -C ' + projectPath + ' pull -f', execCallback);
 });
 
 app.listen(5000, function () {
-	console.log('listening on port 5000')
+    console.log('listening on port 5000')
 });
 
 function execCallback(err, stdout, stderr) {
-	if(stdout) console.log(stdout);
-	if(stderr) console.log(stderr);
+    if(stdout) console.log(stdout);
+    if(stderr) console.log(stderr);
 }
